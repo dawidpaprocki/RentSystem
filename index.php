5592bfce5f8af6cdfa52dba6e4976f508f456d29
Dawid Paprocki <dawid.paprocki90@gmail.com>
2019-06-04 20:31:00 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+  <title>Jquery Fullcalandar Integration with PHP and Mysql</title>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.4.0/fullcalendar.css" />
+  <link rel="stylesheet" type="text/css" href="style.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-alpha.6/css/bootstrap.css" />
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.18.1/moment.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.4.0/fullcalendar.min.js"></script>
+  <script src="script.js" type="text/javascript"></script>
+  <script src="https://kit.fontawesome.com/548a081175.js"></script>
+</head>
+
+<body>
+  <div class="container">
+    <div class="row" style="margin-top:10%;">
+      <div class="col-3 note-img">
+        <div class="note-text" style=" position:absolute;">
+                <h2>Pomoc</h2>
+          <ul class="fa-ul">
+            <li><i class="fa-li fa fa-check"></i><span>W celu dodania nowego elementu zaznacz rubrykę oraz przeciągnij na odpowiednią ilośc godziń gdzie chcesz dodc rezerwacje</span></li>
+            <li><i class="fa-li fa fa-check"></i><span>Żeby skasowac element wystarczy go kliknąc oraz potwierdzic chec usuniecia</span></li>
+            <li><i class="fa-li fa fa-check"></i><span>Edytowac rezerwacje można poprzez najechanie myszką na element, pojawią się 3 paski wystarczy je przeciągnąc.</span></li>
+          </ul>
+        </div>
+        <div style="    margin-left: -60%;  margin-top: -20%;">
+
+          <img src="note.png" height="700" width="700">
+        </div>
+      </div>
+    </div>
+  </div>
+</body>
+33
+</html>
